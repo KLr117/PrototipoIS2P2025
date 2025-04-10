3ae9365eac7e981d8aa8f5d042f80860a71acb01
KLr117 <rl62138@gmail.com>
2025-04-10 10:51:43 -0600

    scriptactualizado

--- Scripts/Parcial2-KevinLopez/Peliculas_mantenimiento.sql
+++ Scripts/Parcial2-KevinLopez/Peliculas_mantenimiento.sql
@@ -2,6 +2,7 @@ use colchoneria;
 
 -- tabla usada para el mantenimiento
 
+DROP TABLE IF EXISTS Peliculas;
 CREATE TABLE IF NOT EXISTS `Peliculas` (
   `idPeliculas` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NULL,
@@ -10,6 +11,7 @@ CREATE TABLE IF NOT EXISTS `Peliculas` (
   `Subtitulado` VARCHAR(45) NULL,
   `Idioma` VARCHAR(45) NULL,
   `precio` DOUBLE NULL,
+  estado TINYINT(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`idPeliculas`)
   )
 ENGINE = InnoDB;
